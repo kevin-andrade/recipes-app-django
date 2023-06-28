b1bc8b68fcdba130a5e4288b91f2116ea9335c0c
Keven Andrade <nivekandrade@live.com>
2023-06-28 13:42:40 -0300

    add id in href

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-# from recipes.views import home, contato, sobre
 from . import views
+
 
 app_name = 'recipes'
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('recipes/<int:id>/', views.recipe, name="recipe"),
+    path('recipes/category/<int:category_id>/', views.category, name="category"),
 ]
