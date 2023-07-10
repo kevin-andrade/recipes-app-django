927e521d033a2db2930e956dee9afb2e280cc450
Keven Andrade <nivekandrade@live.com>
2023-07-10 20:42:37 -0300

    started pagination

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,10 @@
 from django.http import Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 # from utils.recipes.factory import make_recipe
-from recipes.models import Recipe
+from django.core.paginator import Paginator
 from django.db.models import Q
+
+from recipes.models import Recipe
 
 
 def home(request):
@@ -10,8 +12,13 @@ def home(request):
     recipes = Recipe.objects.filter(
         is_published=True).order_by('-id')
 
+    current_page = request.GET.get('page', 1)
+    paginator = Paginator(recipes, 9)
+    page_obj = paginator.get_page(current_page)
+
+    
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
     })
 
 
