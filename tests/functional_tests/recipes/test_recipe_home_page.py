12fcb7f25d47b0a0af33749e7c773441d86887c9
Keven Andrade <nivekandrade@live.com>
2023-07-27 15:49:16 -0300

    test functional home page pagination

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -18,27 +18,53 @@ class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
         # Need to create a recipe
         recipes = self.make_recipe_in_batch()
 
-        title_needed = "This is what I need"
+        title_needed = "This is the recipe I want"
         recipes[0].title = title_needed
         recipes[0].save()
 
         # user opens the page
         self.browser.get(self.live_server_url)
 
-        # see a search field named "Search for a recipe"
+        # see a search field named "Type to search for a recipe"
         search_input = self.browser.find_element(
             By.XPATH,
             '//input[@placeholder="Type to search for a recipe"]'
-            )
+        )
 
         # click on this input and type the desired title
         search_input.send_keys(title_needed)
         search_input.send_keys(Keys.ENTER)
 
+        self.sleep(5)
+
         # user see what he was looking for
         self.assertIn(
-            title_needed,
+            title_needed, 
             self.browser.find_element(By.TAG_NAME, 'body').text
         )
+        self.assertIn(
+            title_needed,
+            self.browser.find_element(By.CLASS_NAME, 'main-content-list').text
+        )
 
-        self.sleep(10)
+    @patch('recipes.views.PER_PAGE', new=2)
+    def test_home_page_pagination(self):
+        # Create a recipe
+        self.make_recipe_in_batch()
+
+        # User open the page
+        self.browser.get(self.live_server_url)
+
+        # User see that it has pagination and click on the second page
+        page2 = self.browser.find_element(
+            By.XPATH,
+            '//a[@aria-label="Go to page 2"]'
+        )
+        page2.click()
+
+        # User see there are 2 more recipes on page 2
+        self.assertEqual(
+            len(self.browser.find_elements(By.CLASS_NAME, 'recipe')), 2
+        )
+
+        self.sleep(8)
