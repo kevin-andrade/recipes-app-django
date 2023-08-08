ff4b7e29be682c6c3133eda0bdfe36f5e2a0e84b
Keven Andrade <nivekandrade@live.com>
2023-08-08 15:48:49 -0300

    Reusing the CBV to generate new recipes

--- authors/views/all.py
+++ authors/views/all.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 
 from recipes.models import Recipe
-from ..forms import RegisterForm, LoginForm, AuthorRecipeForm
+from ..forms import RegisterForm, LoginForm
 
 
 def register_view(request):
@@ -97,72 +97,6 @@ def dashboard(request):
         'authors/pages/dashboard.html',
         context={
             'recipes': recipes,
-        }
-    )
-
-
-@login_required(redirect_field_name="next", login_url='authors:login')
-def dashboard_recipe_edit(request, id):
-    recipe = Recipe.objects.filter(
-        is_published=False,
-        author=request.user,
-        pk=id,
-    ).first()
-
-    if not recipe:
-        raise Http404()
-
-    form = AuthorRecipeForm(
-        data=request.POST or None,
-        files=request.FILES or None,
-        instance=recipe,
-    )
-
-    if form.is_valid():
-        recipe = form.save(commit=False)
-
-        recipe.author = request.user
-        recipe.preparation_steps_is_html = False
-        recipe.is_published = False
-
-        form.save()
-        messages.success(request, 'Your recipe has been successfully saved!')
-        return redirect(reverse('authors:dashboard_recipe_edit', args=(id,)))
-
-    return render(
-        request,
-        'authors/pages/dashboard_recipe.html',
-        context={
-            'form': form,
-        }
-    )
-
-
-@login_required(redirect_field_name="next", login_url='authors:login')
-def dashboard_recipe_new(request):
-    form = AuthorRecipeForm(
-        data=request.POST or None,
-        files=request.FILES or None,
-    )
-
-    if form.is_valid():
-        recipe = form.save(commit=False)
-
-        recipe.author = request.user
-        recipe.preparation_steps_is_html = False
-        recipe.is_published = False
-
-        form.save()
-        messages.success(request, 'Your recipe has been successfully saved!')
-        return redirect(
-            reverse('authors:dashboard_recipe_edit', args=(recipe.id,)))
-
-    return render(
-        request,
-        'authors/pages/dashboard_recipe.html',
-        context={
-            'form': form,
-            'form_action': reverse('authors:dashboard_recipe_new')
         }
     )
 
