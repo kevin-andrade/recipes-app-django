2c4d4ef3fd6adc8338ad2431b07eefe2ca7de2b0
Keven Andrade <nivekandrade@live.com>
2023-08-24 18:37:51 -0300

    Fix validation model Recipes

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,6 @@
+from collections import defaultdict
 from django.contrib.auth.models import User
-from django.contrib.contenttypes.fields import GenericRelation
+from django.forms import ValidationError
 from tag.models import Tag
 from django.db import models
 from django.urls import reverse
@@ -48,3 +49,18 @@ class Recipe(models.Model):
         self.slug = slug
 
         return super().save(*args, **kwargs)
+
+    def clean(self, *args, **kwargs):
+        error_messages = defaultdict(list)
+
+        recipe_from_db = Recipe.objects.filter(
+            title__iexact=self.title
+        ).first()
+
+        if recipe_from_db:
+            if recipe_from_db.pk != self.pk:
+                error_messages['title'].append(
+                    'Found recipes with the same title')
+
+        if error_messages:
+            raise ValidationError(error_messages)
