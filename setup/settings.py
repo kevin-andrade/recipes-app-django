4a036ceb43317fc1e3b5ce96ddc719691ff50bf0
Keven Andrade <nivekandrade@live.com>
2023-08-24 15:55:18 -0300

    Fix name ALLOWED_HOSTS correct

--- setup/settings.py
+++ setup/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = str(os.getenv('SECREY_KEY'))
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
 ALLOWED_HOSTS: list[str] = parse_comma_sep_str_to_list(
-    get_env_variable('ALLOWED_HOST')
+    get_env_variable('ALLOWED_HOSTS')
 )
 
 CSRF_TRUSTED_ORIGINS: list[str] = parse_comma_sep_str_to_list(
