5c40a1e707bbaa9d6fb2c17a1069309128ad75ac
Keven Andrade <nivekandrade@live.com>
2023-08-29 16:42:14 -0300

    Resizing images in model save (Recipe cover)

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,4 @@
+import os
 from collections import defaultdict
 from django.contrib.auth.models import User
 from django.forms import ValidationError
@@ -6,6 +7,8 @@ from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+from PIL import Image
 
 
 class Category(models.Model):
@@ -47,11 +50,37 @@ class Recipe(models.Model):
     def get_absolute_url(self):
         return reverse("recipes:recipe", args=(self.id,))
 
+    def resize_image(image, new_width=800):
+        image_full_path = os.path.join(settings.MEDIA_ROOT, image.name)
+        image_pillow = Image.open(image_full_path)
+        original_width, original_height = image_pillow.size
+
+        if original_width <= new_width:
+            image_pillow.close()
+            return
+
+        new_height = round((new_width * original_height) / original_width)
+
+        new_image = image_pillow.resize((new_width, new_height), Image.LANCZOS)
+        new_image.save(
+            image_full_path,
+            optimize=True,
+            quality=60,
+        )
+
     def save(self, *args, **kwargs):
         slug = f'{slugify(self.title)}'
         self.slug = slug
 
-        return super().save(*args, **kwargs)
+        saved = super().save(*args, **kwargs)
+
+        if self.cover:
+            try:
+                self.resize_image(self.cover, 840)
+            except (FileNotFoundError):
+                ...
+
+        return saved
 
     def clean(self, *args, **kwargs):
         error_messages = defaultdict(list)
