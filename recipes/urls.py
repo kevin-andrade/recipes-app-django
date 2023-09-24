b35daa54853f3b50393082605bcdf7022b0a1f2a
Keven Andrade <nivekandrade@live.com>
2023-09-24 11:26:53 -0300

    Add SimpleRouter in RecipeAPIv2 urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
 from recipes import views
 
 
 app_name = 'recipes'
+recipe_api_v2_router = SimpleRouter()
+recipe_api_v2_router.register(
+    'recipes/api/v2',
+    views.RecipeAPIv2ViewSet,
+    basename='recipes-api'
+)
 
 urlpatterns = [
     path(
@@ -41,24 +48,9 @@ urlpatterns = [
         name="recipe_api_v1_detail"),
 
     path(
-        'recipes/api/v2/',
-        views.RecipeAPIv2ViewSet.as_view({
-            'get': 'list',
-            'post': 'create'
-        }),
-        name="recipe_api_v2"),
-
-    path(
-        'recipes/api/v2/<int:pk>/',
-        views.RecipeAPIv2ViewSet.as_view({
-            'get': 'retrieve',
-            'patch': 'partial_update',
-            'delete': 'destroy',
-        }),
-        name="recipe_api_v2_detail"),
-
-    path(
         'recipes/api/v2/tag/<int:pk>/',
         views.tag_api_detail,
         name="recipe_api_v2_tag"),
+
+    path('', include(recipe_api_v2_router.urls)),
 ]
